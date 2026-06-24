241346b2cb2cd91e3912233a7321cdf01de62eb5
Garv <garvkabir0615@gmail.com>
2026-06-24 17:01:08 +0530

    Cleanup: remove old deleted files

---
+++ 01 Start Basic.py
@@ -0,0 +1,11 @@
+#print statement     # its a comment
+print("hello world")
+#escape sequence by /n
+print("my name is garv/nmy age is 20")
+# sep and end
+print("i love coding",4.5,6,sep="~",end="...")
+# pip is a package manager which installs external modules to our python environment
+#Two types of modules first is built in modules second is external modules
+
+
+
