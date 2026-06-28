4b6fbdbe6f3771535840f638bc504cd62c38071a
Garv <garvkabir0615@gmail.com>
2026-06-28 18:45:27 +0530

    Docstring.py

---
+++ Python learning/Docstring.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the value"))
+def root(n):
+    '''This is n and it will show
+    the square root of n number'''
+    return(n**0.5)
+print(root.__doc__)
+
+print(root(num))
+
